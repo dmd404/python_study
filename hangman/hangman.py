cd9aa75179eec527e43d4a78d6b345a7fe9b1039
dmd404 <dpfj484@gmail.com>
2023-03-20 15:40:03 +0900

    replace blanks to guessed letter if correct

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,17 +1,22 @@
-# Step 1
 import random
-
 word_list = ["aardvark", "baboon", "camel"]
-
-# Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
 
-# Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input('Guess a letter: ').lower()
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
 
-# Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-for i in chosen_word:
-    if i == guess:
-        print('correct')
-    else:
-        print('wrong')
+#For each letter in the chosen_word, add a "_" to 'display'.
+display = []
+for letter in chosen_word:
+  display.append('_')
+
+guess = input("Guess a letter: ").lower()
+
+
+#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+for i, letter in enumerate(chosen_word):
+  if letter == guess:
+    display[i] = letter
+
+#Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+print(display)
