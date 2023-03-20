b76581feedf62d8709d6e661e24af52e659c51b3
dmd404 <dpfj484@gmail.com>
2023-03-20 16:04:45 +0900

    count user's lives

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,28 +1,94 @@
 import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+end_of_game = False
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 
+lives = 6
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-#For each letter in the chosen_word, add a "_" to 'display'.
+#create blanks
 display = []
 for letter in chosen_word:
     display.append('_')
 
-# Use a while loop to let the user guess again.
-end_of_game = False
-
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
-
+    # check guessed letter
     for i, letter in enumerate(chosen_word):
         if letter == guess:
             display[i] = letter
 
+    # If guess is not a letter in the chosen_word, then reduce 'lives' by 1.
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print('You lose')
+
     print(display)
+    print(f'lives: {lives}')
 
     # check if the user has got all letters
     if "_" not in display:
         end_of_game = True
         print('You win')
+
+    print(stages[lives])
