1c77b7e00a237d41e0b17565c978c9093dd61931
dmd404 <106816837+dmd404@users.noreply.github.com>
2023-03-21 16:31:39 +0900

    combined the encrypt() and decrypt() function

--- caesar_cipher/caesar.py
+++ caesar_cipher/caesar.py
@@ -4,30 +4,15 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text, shift_amount):
-  encrypted_txt = ""
-  for letter in plain_text:
+def caesar(start_text, shift_amount, cipher_direction):
+  final_text = ""
+  for letter in start_text:
     original = alphabet.index(letter)
-    new_position = (original + shift_amount) % 26  # in case index runs out of range
-    new_letter = alphabet[new_position]
-    encrypted_txt += new_letter
-  
-  print(encrypted_txt)
+    if direction == 'decode':
+      shift_amount *= -1
+    new_position = (original + shift_amount) % 26
+    final_text += alphabet[new_position]
+    
+  print(f'The {cipher_direction}d text is {final_text}')
 
-# Create a decrypt function 
-def decrypt(encrypted_text, shift_amount):
-  decrypted_txt = ""
-  # shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
-  for letter in encrypted_text:
-    position = alphabet.index(letter)
-    new_position = (position - shift_amount) % 26
-    decrypted_txt += alphabet[new_position]
-  print(decrypted_txt)
-  
-
-# Check if the user wanted to encrypt or decrypt the message 
-if direction == 'encode':
-  encrypt(text, shift)
-elif direction == 'decode':
-  decrypt(text, shift)
-                           
+caesar(text, shift, direction)
