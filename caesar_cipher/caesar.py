5a30c16d48f5a77a4b264be745b7f6774cefe4be
dmd404 <106816837+dmd404@users.noreply.github.com>
2023-03-21 16:47:29 +0900

    improved user experience

--- caesar_cipher/caesar.py
+++ caesar_cipher/caesar.py
@@ -9,12 +9,24 @@ def caesar(start_text, shift_amount, cipher_direction):
     final_text = ""
     if direction == 'decode':
         shift_amount *= -1
-    for letter in start_text:
-        original = alphabet.index(letter)
-        new_position = (original + shift_amount) % 26
-        final_text += alphabet[new_position]
+    for char in start_text:
+        if char in alphabet:
+            original = alphabet.index(letter)
+            new_position = (original + shift_amount) % 26
+            final_text += alphabet[new_position]
+        else:
+            end_text += char
     
     print(f'The {cipher_direction}d text is {final_text}')
 
-    
-caesar(text, shift, direction)
+rerun = True
+while rerun:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  user_rerun = input('continue? ').lower()
+  if user_rerun == 'no':
+    rerun = False
+    print('Bye')
