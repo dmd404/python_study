a99e83e9ad63723d657bcbec4c8b77c3c9326a67
dmd404 <106816837+dmd404@users.noreply.github.com>
2023-03-21 16:33:42 +0900

    fixed encode, decode bug
    took if statement outside of for loop

--- caesar_cipher/caesar.py
+++ caesar_cipher/caesar.py
@@ -4,15 +4,17 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def caesar(start_text, shift_amount, cipher_direction):
-  final_text = ""
-  for letter in start_text:
-    original = alphabet.index(letter)
-    if direction == 'decode':
-      shift_amount *= -1
-    new_position = (original + shift_amount) % 26
-    final_text += alphabet[new_position]
-    
-  print(f'The {cipher_direction}d text is {final_text}')
 
+def caesar(start_text, shift_amount, cipher_direction):
+    final_text = ""
+    if direction == 'decode':
+        shift_amount *= -1
+    for letter in start_text:
+        original = alphabet.index(letter)
+        new_position = (original + shift_amount) % 26
+        final_text += alphabet[new_position]
+    
+    print(f'The {cipher_direction}d text is {final_text}')
+
+    
 caesar(text, shift, direction)
