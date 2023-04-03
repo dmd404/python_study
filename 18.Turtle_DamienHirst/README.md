c1153ca9e829d94780a8b7e0cbfece27e8a4b8c6
dmd404 <dpfj484@gmail.com>
2023-04-03 11:07:18 +0900

    create readme file and complete challenge 3

---
+++ 18.Turtle_DamienHirst/README.md
@@ -0,0 +1,13 @@
+# Turtle and GUI
+Create a beautiful dot painting that looks similar to work 
+of Damien Hirst by using a Turtle module.  
+
+Color of dots are randomized.  
+
+
+
+## References
+
+---
+- <a href='https://docs.python.org/3/library/turtle.html' target='_blank'>Turtle Graphics Documentation</a>
+- <a href="https://cs111.wellesley.edu/labs/lab02/colors" target="_blank">Turtle colors rendered</a>
--- 18.Turtle_DamienHirst/main.py
+++ 18.Turtle_DamienHirst/main.py
@@ -1,15 +1,48 @@
 from turtle import Turtle, Screen
-
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape('turtle')  # change shape to turtle
-timmy_the_turtle.color('blue')  # change color
-
-
-
-
-
-
-
+import random
 
 screen = Screen()
+tim = Turtle()
+tim.shape('turtle')  # change shape to turtle
+tim.color('blue')  # change color
+
+# # Challenge 1: draw a square
+# for i in range(4):
+#     tim.fd(100)
+#     tim.rt(90)
+
+
+# # Challenge 2: draw a dashed line
+# for _ in range(10):
+#     tim.fd(10)
+#     tim.pu()
+#     tim.fd(10)
+#     tim.pd()
+#
+#
+# def dashed_line(turtle, num_lines):
+#     for _ in range(num_lines):
+#         turtle.fd(10)
+#         turtle.pu()
+#         turtle.fd(10)
+#         turtle.pd()
+# # dashed_line(tim, 5)
+
+# Challenge 3: Draw different shapes triangle to decagon
+# each of the shape drawn with random color
+# each sides with lengh of 100
+def draw_shape(length, num_sides):
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    tim.color((r, g, b))
+    for _ in range(num_sides):
+        tim.fd(length)
+        tim.rt(360/num_sides)
+
+
+screen.colormode(255)
+for _ in range(3, 11):
+    draw_shape(100, _)
+
 screen.exitonclick()  # window disappears when clicked on
