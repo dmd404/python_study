814cb1ec5465d23b690bb421a8cc2360f16160b3
dmd404 <dpfj484@gmail.com>
2023-04-03 10:10:47 +0900

    practice using turtle

---
+++ 18.Turtle_DamienHirst/main.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape('turtle')  # change shape to turtle
+timmy_the_turtle.color('blue')  # change color
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()  # window disappears when clicked on
