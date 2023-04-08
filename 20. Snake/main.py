7ce78b54b74400c34044746e45d8e9717e7636fa
dmd404 <dpfj484@gmail.com>
2023-04-08 11:03:14 +0900

    update screen the move the snake forward

--- 20. Snake/main.py
+++ 20. Snake/main.py
@@ -4,13 +4,24 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title("Snake Game")
+screen.tracer(0)
 
 x_pos = [0, -20, -40]
+
+segments = []
 
 for _ in x_pos:
     white_square = Turtle(shape='square')
     white_square.penup()
     white_square.color('white')
     white_square.setx(_)
+    segments.append(white_square)
+
+
+game_is_on = True
+while game_is_on:
+    for seg in segments:
+        seg.forward(20)
+        screen.update()
 
 screen.exitonclick()
