848409d936ce9ece0eecf0f3684cdd90229a950e
dmd404 <106816837+dmd404@users.noreply.github.com>
2023-04-13 16:45:25 +0900

    Add files via upload

---
+++ 20. Snake/food.py
@@ -0,0 +1,16 @@
+from turtle import Turtle
+import random
+
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape('circle')
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)  # from 20 by 20 pixels to 10 by 10 circle
+        self.color('blue')
+        self.speed('fastest')
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
