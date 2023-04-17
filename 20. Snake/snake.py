9eb410bba22d255794b4d51550c2383ae70733cf
dmd404 <106816837+dmd404@users.noreply.github.com>
2023-04-18 08:39:12 +0900

    add reset method

--- 20. Snake/snake.py
+++ 20. Snake/snake.py
@@ -27,6 +27,13 @@ class Snake:
         # white_square.setx(position)
         self.segments.append(white_square)
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def extend(self):
         # add a new segment to the snake.
         self.add_segment(self.segments[-1].position())  # add segment as the last segment
