c604f19911d24b647a5dd4b57692857a5fd44133
dmd404 <dpfj484@gmail.com>
2023-04-28 11:28:13 +0900

    set up timer

--- 28. pomodoro/main.py
+++ 28. pomodoro/main.py
@@ -7,34 +7,53 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0
 # ---------------------------- TIMER RESET ------------------------------- #
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text='Break', fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text='Break', fg=PINK)
+    else:
+        count_down(work_sec)
+        title_label.config(text='Work', fg=GREEN)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-
     count_min = math.floor(count / 60)
     count_sec = count % 60
-
+    if count_sec == 0:
+        count_sec = '00'
+    elif count_sec < 10:
+        count_sec = f'0{count_sec}'
     canvas.itemconfig(timer_text, text=f'{count_min}:{count_sec}')
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title('Pomodoro')
 window.config(padx=100, pady=50, bg=YELLOW)
 
-timer_label = Label(text='Timer', bg=YELLOW, fg=GREEN, font=(FONT_NAME, 45))
-timer_label.grid(column=1, row=0)
+title_label = Label(text='Timer', bg=YELLOW, fg=GREEN, font=(FONT_NAME, 45))
+title_label.grid(column=1, row=0)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)  # roughly same size as the image and to work with even number
 tomato_img = PhotoImage(file='tomato.png')
