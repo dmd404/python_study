38c5fa0e28d861d5a7d5254dad42c7eb9c75dbdc
dmd404 <dpfj484@gmail.com>
2023-05-26 16:42:12 +0900

    generate random pwd and easy access with pyperclip

--- 29. password_manager/main.py
+++ 29. password_manager/main.py
@@ -1,8 +1,23 @@
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+    letters_list = [choice(letters) for _ in range(randint(8, 10))]
+    symbols_list = [choice(symbols) for _ in range(randint(2, 4))]
+    numbers_list = [choice(numbers) for _ in range(randint(2, 4))]
+    password_list = letters_list + symbols_list + numbers_list
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    pwd_entry.insert(0, password)
+    pyperclip.copy(password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website = website_entry.get()
@@ -50,7 +65,7 @@ pwd_entry = Entry(width=21)
 pwd_entry.grid(row=3, column=1)
 
 # Button
-pwd_btn = Button(text='Generate Password')
+pwd_btn = Button(text='Generate Password', command=generate_password)
 pwd_btn.grid(row=3, column=2)
 add_btn = Button(text='Add', width=36, command=save)
 add_btn.grid(row=4, column=1, columnspan=2)
