da81825071f73095c9233773432c75efd2bc115e
dmd404 <dpfj484@gmail.com>
2023-06-04 11:10:13 +0900

    raise your own exception

--- 30. json_password_manager/main.py
+++ 30. json_password_manager/main.py
@@ -15,19 +15,33 @@
 #print(text + 5)
 
 # FileNotFound
-try:
-    file = open('a_file.txt')
-    a_dictionary = {'key': 'value'}
-    print(a_dictionary['key'])
-except FileNotFoundError:
-    # print('There was an error')
-    file = open('a_file.txt', 'w')
-    file.write('something')
-except KeyError as error_message:
-    print(f'That key {error_message} does not exist.')
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
-    print('File was closed.')
+#try:
+#    file = open('a_file.txt')
+#    a_dictionary = {'key': 'value'}
+#    print(a_dictionary['key'])
+#except FileNotFoundError:
+#    # print('There was an error')
+#    file = open('a_file.txt', 'w')
+#    file.write('something')
+#except KeyError as error_message:
+#    print(f'That key {error_message} does not exist.')
+#else:
+#    content = file.read()
+#    print(content)
+#finally:
+# #   file.close()
+# #  print('File was closed.')
+# raise TypeError('This is an error that I made up.')
+#
+## raise
+## allows you to raise your own exception
+
+
+height = float(input('Height: '))
+weight = float(input('Weight: '))
+
+if height > 3:
+    raise ValueError('Human height should not be over 3 meters.')
+
+bmi = weight / height ** 2
+print(bmi)
